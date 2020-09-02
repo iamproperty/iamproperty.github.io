3801e8686779d599269c5d71576dd1c495b278b5
James Fenwick <james.fenwick@iamproperty.com>
2020-09-02 11:01:25 +0100

    Separate the documentation app from the component library

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  moduleNameMapper: {
+    '~/(.*)$': '<rootDir>/docs/$1',
+    '@/(.*)$': '<rootDir>/src/$1'
+  }
 }
