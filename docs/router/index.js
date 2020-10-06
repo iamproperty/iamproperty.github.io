12782a6ae2a03ea94e1e06b645ed92904132226e
James Fenwick <james.fenwick@iamproperty.com>
2020-10-06 11:20:20 +0100

    Move documentation views to docs directory

--- docs/router/index.js
+++ docs/router/index.js
@@ -16,22 +16,29 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Components.vue')
-  },
-  {
-    path: '/components/task-title',
-    name: 'Task Title',
-    component: () => import('../views/TaskTitle.vue')
-  },
-  {
-    path: '/components/key-stats',
-    name: 'Key Stats',
-    component: () => import('../views/KeyStats.vue')
-  },
-  {
-    path: '/components/property-task-intro',
-    name: 'Property Task Intro',
-    component: () => import('../views/PropertyTaskIntro.vue')
+    component: () => import(/* webpackChunkName: "components" */ '../views/Components.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Index',
+        component: () => import('../views/components/Index.vue')
+      },
+      {
+        path: 'task-title',
+        name: 'Task Title',
+        component: () => import('../views/components/TaskTitle.vue')
+      },
+      {
+        path: 'key-stats',
+        name: 'Key Stats',
+        component: () => import('../views/components/KeyStats.vue')
+      },
+      {
+        path: 'property-task-intro',
+        name: 'Property Task Intro',
+        component: () => import('../views/components/PropertyTaskIntro.vue')
+      }
+    ]
   }
 ]
 
---
+++ docs/views/components/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="mb5">
+    <h3>Component table of contents</h3>
+    <hr>
+    <ul class="component-contents">
+      <li>
+        <router-link to="/components/task-title">Task Title</router-link>
+        - The task title component has a number of choices for background colours
+      </li>
+      <li>
+        <router-link to="/components/key-stats">Key Fact Group</router-link>
+        - Group a series of key facts together in a responsive grid
+      </li>
+      <li>
+        <router-link to="/components/property-task-intro">Property Task Intro</router-link>
+        - Group of property data including: address, starting bid, auction start, auction end date
+        and branch details
+      </li>
+    </ul>
+  </section>
+</template>
+
+<style scoped>
+.component-contents li {
+  display: block;
+  margin-bottom: 10px;
+}
+</style>
