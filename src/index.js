9346caf162002ec0f8c84b178a3fe91f3b34fd3c
James Fenwick <james.fenwick@iamproperty.com>
2020-10-08 15:01:18 +0100

    Export components for library

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as KeyFact } from './components/KeyFact.vue'
+export { default as KeyFactGroup } from './components/KeyFactGroup.vue'
+export { default as PropertyTaskIntro } from './components/PropertyTaskIntro.vue'
+export { default as TaskTitle } from './components/TaskTitle.vue'
