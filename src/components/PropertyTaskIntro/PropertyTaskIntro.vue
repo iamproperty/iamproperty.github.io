4bd5ed4f44e8c18b5ae4eae8a3578d5504137f4b
Phil Franklin <phil.franklin@iamproperty.com>
2020-10-13 10:45:36 +0100

    Update PropertyTaskIntro Component To Make Additional Info Optional With Props And Amend Styling

--- src/components/PropertyTaskIntro/PropertyTaskIntro.vue
+++ src/components/PropertyTaskIntro/PropertyTaskIntro.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="property-task-intro">
-      <h4><a :href="property.url">{{ property.address }}</a></h4>
-      <h4>Starting Bid: £{{property.startingBid}}</h4>
-      <p>Contract signed: {{property.contractSigned}} <span>{{ property.contractTime }}</span></p>
-      <ul class="auction-info d-sm-flex">
+    <div class="row">
+      <div class="col-12">
+        <h2>{{ property.address }}</h2>&nbsp;<a :href="property.url" target="_blank">View details</a>
+      </div>
+      <div class="col-12" v-if="branch">
+        <ul class="branch-info d-sm-flex">
+          <li><strong>{{property.branchName}}</strong></li>
+          <li>Telephone: <a :href="`tel: ${property.branchTelephone}`">{{property.branchTelephone}}</a></li>
+          <li>Email: <a :href="`mailto:  ${property.branchEmail}`">{{property.branchEmail}}</a></li>
+        </ul>
+      </div>
+      <div class="col-12" v-if="bid">
+        <ul class="bid-info d-sm-flex" >
+          <li>Starting Bid: £{{property.startingBid}}</li>
+          <li>Contract signed: {{property.contractSigned}} <span>{{ property.contractTime }}</span></li>
+        </ul>
+      </div>
+      <div class="col-12" v-if="auction">
+        <ul class="auction-info d-sm-flex">
           <li>Auction start date: {{property.auctionStartDate}}</li>
           <li>Auction end date: {{property.auctionEndDate}}</li>
-      </ul>
-      <ul class="branch-info d-sm-flex">
-        <li><strong>{{property.branchName}}</strong></li>
-        <li>Telephone: <a :href="`tel: ${property.branchTelephone}`">{{property.branchTelephone}}</a></li>
-        <li>Email: <a :href="`mailto:  ${property.branchEmail}`">{{property.branchEmail}}</a></li>
-      </ul>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
   name: 'PropertTaskIntro',
@@ -22,36 +33,42 @@ export default {
     property: {
       type: Object,
       required: true
-    }
+    },
+    auction: {
+      type: Boolean,
+      default: false
+    },
+    bid: {
+      type: Boolean,
+      default: false
+    },
+    branch: {
+      type: Boolean,
+      default: false
+    },
   }
 }
 </script>
 <style lang="scss">
+.property-task-intro {
+  span {
+    color:red;
+    padding-left: 10px;
+  }
+}
 
-    .property-task-intro {
-        span {
-            color:red;
-            padding-left: 10px;
-        }
-        a {
-          color: #ffa500;
-        }
+ul.auction-info, ul.bid-info, ul.branch-info {
+  list-style: none;
+  li {
+    padding-left: 15px;
+
+    &:first-of-type {
+      padding-left: 0;
     }
 
-    ul.auction-info, ul.branch-info {
-        list-style: none;
-
-        li {
-            padding-left: 15px;
-
-            &:first-of-type {
-                padding-left: 0;
-            }
-
-            @media only screen and (max-width: 576px) {
-                padding-left: 0;
-            }
-
-        }
+    @media only screen and (max-width: 576px) {
+      padding-left: 0;
     }
+  }
+}
 </style>
