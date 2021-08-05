4d479b12e252863850dcb592fe7fd2ae808c9ec8
JamesLambert1986 <jameslambert1986@gmail.com>
2021-08-05 15:24:30 +0100

    Sass unit tests
    Investigating a way to test the styles of a component, I have managed to get Sass true to work. But so far can not find a way to use it to test the specific styles of a component.
    
    DS-9

--- tests/unit/logos.spec.js
+++ tests/unit/logos.spec.js
@@ -1,12 +1,56 @@
 import { shallowMount } from '@vue/test-utils'
 import Logo from '@/foundations/Logo/Logo.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(Logo, {
-      propsData: { msg }
+describe('Logo component', () => {
+  it('renders the iam property logo by default', () => {
+    const test = shallowMount(Logo, {
+      propsData: { }
     })
-    expect(wrapper.text()).toMatch('For a guide')
+    
+    expect(test.html()).toContain('<title>iam property</title>')
+    expect(test.html()).toContain('<use xlink:href="#logo-property"></use>')
+  })
+
+  it('renders the correct logo when an ID is passed', () => {
+    const test = shallowMount(Logo, {
+      propsData: {
+        id: 'sold'
+      }
+    })
+    
+    expect(test.html()).toContain('<title>iam sold</title>')
+    expect(test.html()).toContain('<use xlink:href="#logo-sold"></use>')
+  })
+
+  it('renders a description when passed', () => {
+    const test = shallowMount(Logo, {
+      propsData: { }
+    })
+    
+    expect(test.html()).not.toContain('<span></span>')
+
+    const test2 = shallowMount(Logo, {
+      propsData: {
+        desc: 'description'
+      }
+    })
+    
+    expect(test2.html()).toContain('<span>description</span>')
+  })
+
+  it('background can be modified', () => {
+    const test = shallowMount(Logo, {
+      propsData: { }
+    })
+    
+    expect(test.html()).not.toContain('<span></span>')
+
+    const test2 = shallowMount(Logo, {
+      propsData: {
+        desc: 'description'
+      }
+    })
+    
+    expect(test2.html()).toContain('<span>description</span>')
   })
 })
