8d44a8050e37837d8aa037100ca3bf6faa08e38e
JamesLambert1986 <jameslambert1986@gmail.com>
2021-08-09 13:57:53 +0100

    Minor adjustments and documentation
    DS-18

--- docs/views/elements/Type.vue
+++ docs/views/elements/Type.vue
@@ -2,6 +2,9 @@
   <main>
     <div class="container">
       <h1>Typography</h1>
+      <p>All of our typographic elements should be sized using relative units to its font size. This will reduce the amount of bespoke CSS for each header and created more robost elements.</p>
+      <p>Our base font size is 16px or 1rem and any other rem values are calculated by multiplying the base font size. Em values are different as these are multiplied by the font-size of the element they are being applied too. So a margin bottom of 0.5em on a heading element with the font size of 32px will work out to be 16px.</p>
+      <h2>Scale</h2>
       <table>
         <thead>
           <tr>
@@ -15,7 +18,7 @@
         </thead>
         <tbody>
           <tr>
-            <td><span class="h1">heading 1</span></td>
+            <td><span class="h1 text-nowrap">heading 1</span></td>
             <td>{{$shared.cssVars.__font_heading}}</td>
             <td>{{$shared.cssVars.__heading_weight}}</td>
             <td><span>{{$shared.cssVars.__fs_1}}</span>/<span>2.5rem</span></td>
@@ -66,9 +69,19 @@
       </table>
     </div>
     <div class="container">
-      <h6>Heading number 1<br/>Heading number 1</h6>
-      <p class="h1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      <h2>Body Text</h2>
+      <h3>Default</h3>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <h3>.lead</h3>
+      <p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      <h3>.strapline</h3>
+      <span class="d-block h1">Heading 1</span>
+      <p class="strapline">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      <h3>Blockquote</h3>
+      <blockquote>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        <cite>Citation</cite>
+      </blockquote>
     </div>
   </main>
 </template>
