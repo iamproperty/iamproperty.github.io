0c29726981f57d167b87f08eca07da4de150a2e1
JamesLambert1986 <jameslambert1986@gmail.com>
2021-08-20 15:50:51 +0100

    Start of sortable tables
    DS-93
    DS-44

---
+++ docs/views/elements/AdvancedTables.vue
@@ -0,0 +1,113 @@
+<template>
+  <main>
+    <div class="container pb-0">
+      <h1>Advanced tables</h1>
+      <p>-</p>
+    </div>
+
+    <div class="container visualtest">
+      <Table :fields="sortable_fields" :items="items" sortBy="Job"></Table>
+    </div>
+
+
+    <div class="container">
+      <h2>HTML code examples</h2>
+      <pre v-highlightjs><code class="javascript">{{htmlUsage}}</code></pre>
+    </div>
+
+
+    <div class="container">
+      <h2>Vue Table component reference</h2>
+      <Readme></Readme>
+    </div>
+  </main>
+</template>
+
+<script>
+import Table from '@/elements/Table/Table.vue'
+import Readme from '@/elements/Table/README.md'
+import * as helpers from '../../../assets/js/modules/helpers'
+
+export default {
+  components: {
+    Table,
+    Readme
+  },
+  mounted(){
+    
+    helpers.checkElements(document.querySelector('main'));
+  },
+  data () {
+    return {
+      htmlUsage: ``,
+      sortable_fields: [
+        { 
+          key: 'name',
+          sortable: true
+        },
+        { 
+          key: 'job',
+          sortable: true 
+        },
+        { key: 'address' },
+        { 
+          key: 'emergency_contact',
+          sortable: true 
+        },
+        { key: 'actions' }
+      ],
+      fields: [
+        { key: 'name' },
+        { key: 'job' },
+        { key: 'address' },
+        { key: 'emergency_contact' },
+        { key: 'actions' }
+      ],
+      items: [
+        {
+          name: 'Derrick',
+          job: 'Electrician',
+          address: '5 King Street<br> London<br> London<br> SW20 0AL<br> United Kingdom',
+          emergency_contact: 'Susan',
+          actions: '<a href="#">View</a><br><a href="#">Point of Contact</a>'
+        },
+        {
+          name: 'Andrew2',
+          job: 'Plumber',
+          address: '123 Royal Street<br> London<br> London<br> SW20 0AL<br> United Kingdom',
+          emergency_contact: 'Rachel',
+          actions: '<a href="#">View</a><br><a href="#">Point of Contact</a>'
+        },
+        {
+          name: 'Rachel',
+          job: 'Plumber',
+          address: '123 Royal Street<br> London<br> London<br> SW20 0AL<br> United Kingdom',
+          emergency_contact: 'Susan',
+          actions: '<a href="#">View</a><br><a href="#">Point of Contact</a>'
+        },
+        {
+          name: 'Richard',
+          job: 'Plumber',
+          address: '123 Royal Street<br> London<br> London<br> SW20 0AL<br> United Kingdom',
+          emergency_contact: 'Susan',
+          actions: '<a href="#">View</a><br><a href="#">Point of Contact</a>'
+        },
+        {
+          name: 'Edward',
+          job: 'Plumber',
+          address: '123 Royal Street<br> London<br> London<br> SW20 0AL<br> United Kingdom',
+          emergency_contact: 'Susan',
+          actions: '<a href="#">View</a><br><a href="#">Point of Contact</a>'
+        },
+        {
+          name: 'Sharon',
+          job: 'Plumber',
+          address: '123 Royal Street<br> London<br> London<br> SW20 0AL<br> United Kingdom',
+          emergency_contact: 'Susan',
+          actions: '<a href="#">View</a><br><a href="#">Point of Contact</a>'
+        }
+      ]
+    }
+  }
+}
+</script>
--- src/elements/Table/Table.vue
+++ src/elements/Table/Table.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="table__wrapper">
+  <div class="table__wrapper" ref="wrapper" :data-sortBy="sortBy">
     <table>
       <thead v-if="fields">
         <tr>
-          <th v-for="(field) in fields" :key="field.key">{{ cellHeading(field.key) }}</th>
+          <th v-for="(field) in fields" :key="field.key" :data-sortable="field.sortable" >{{ cellHeading(field.key) }}</th>
         </tr>
       </thead>
       <tbody v-if="items">
@@ -17,10 +17,15 @@
 
 <script>
 import { ucfirst, unsnake } from '../../helpers/strings'
+import table from '../../../assets/js/modules/table.js'
 
 export default {
   name: 'Table',
   props: {
+    sortBy: {
+      type: String,
+      required: false
+    },
     items: {
       type: Array,
       required: true
@@ -36,6 +41,12 @@ export default {
         return `${ucfirst(unsnake(heading))}`
       }
     }
+  },
+  mounted(){
+
+    console.log(this.$el)
+
+    let advancedTable = new table(this.$el);
   }
 }
 </script>
