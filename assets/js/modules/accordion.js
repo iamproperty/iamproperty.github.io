9e5e680f3c79edf9575f84f246e27df9d9f4020b
JamesLambert1986 <jameslambert1986@gmail.com>
2021-08-31 21:49:52 +0100

    Create component
    DS-64

---
+++ assets/js/modules/accordion.js
@@ -0,0 +1,19 @@
+function accordion(accordionElement) {
+  
+  // Fetch all the details element.
+  const details = accordionElement.querySelectorAll("details");
+
+  // Add the onclick listeners.
+  details.forEach((targetDetail) => {
+    targetDetail.addEventListener("click", () => {
+      // Close all the details that are not targetDetail.
+      details.forEach((detail) => {
+        if (detail !== targetDetail) {
+          detail.removeAttribute("open");
+        }
+      });
+    });
+  });
+}
+
+export default accordion
---
+++ src/components/Accordion/Accordion.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container accordion" ref="wrapper">
+    <slot></slot>
+    <details class="accordion-item" v-for="(value,index) in items" :key="index">
+      <summary><span class="accordion-header accordion-button h4">{{value.summary}}</span></summary>
+      <div class="accordion-body" v-html="value.detail"></div>
+    </details>
+    <slot name="after"></slot>
+  </div>
+</template>
+
+<style lang="scss">
+@import "../../../assets/sass/_func.scss";
+@import "../../../assets/sass/components/accordion.scss";
+
+</style>
+
+<script>
+import { ucfirst, unsnake } from '../../helpers/strings'
+import accordion from '../../../assets/js/modules/accordion.js'
+
+export default {
+  name: 'Accordion',
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+  },
+  mounted(){
+    
+    accordion(this.$refs.wrapper);
+  }
+}
+</script>
