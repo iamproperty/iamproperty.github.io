6fc505a4322d5108a9635645c4efc8ea96f1ba03
JamesLambert1986 <jameslambert1986@gmail.com>
2021-11-24 14:59:25 +0000

    Lists documentation

---
+++ docs/views/elements/Lists.vue
@@ -0,0 +1,39 @@
+<template>
+  <main>
+    <div class="container">
+      <ul class="breadcrumb mb-0 d-sm-none">
+        <li><a href="/elements">Elements</a></li>
+      </ul>
+      <h1>Lists</h1>
+      <p>Our lists have been kept very basic using default styles.</p>
+    </div>
+    <div class="container visualtest">
+      <h2>Unordered lists</h2>
+      <ul>
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 3</li>
+        <li>List item 4</li>
+      </ul>
+    </div>
+    <div class="container visualtest">
+      <h2>Ordered lists</h2>
+      <ol>
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 3</li>
+        <li>List item 4</li>
+      </ol>
+    </div>
+    <div class="container visualtest">
+      <h2>Breadcrumb</h2>
+      <ol class="breadcrumb">
+        <li><a href="/home">Home</a></li>
+        <li><a href="/section">Section</a></li>
+        <li><a href="/sub-section">Sub Section</a></li>
+      </ol>
+    </div>
+    
+
+  </main>
+</template>
---
+++ tests/lists.visual.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer')
+const { toMatchImageSnapshot } = require('jest-image-snapshot')
+expect.extend({ toMatchImageSnapshot })
+
+const pkg = require('../package.json');
+
+describe(`Lists page`, () => {
+  it(`It should render correctly `, async() => {
+
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(pkg.localURL+'/foundations/lists#visualtest')
+
+    await page.setViewport({ width: 375, height: 800 })
+    const mobileImage = await page.screenshot({ fullPage: true });
+    expect(mobileImage).toMatchImageSnapshot({ allowSizeMismatch: true, customDiffConfig: { threshold: 0.5 } });
+
+    await page.setViewport({ width: 768, height: 800 })
+    const tabletImage = await page.screenshot({ fullPage: true });
+    expect(tabletImage).toMatchImageSnapshot({ allowSizeMismatch: true, customDiffConfig: { threshold: 0.5 } });
+
+    await page.setViewport({ width: 1440, height: 800 })
+    const desktopImage = await page.screenshot({ fullPage: true });
+    expect(desktopImage).toMatchImageSnapshot({ allowSizeMismatch: true, customDiffConfig: { threshold: 0.5 } });
+
+    await browser.close()
+  });
+});
