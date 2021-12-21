5dd91659a928ce1a173b607ded29517924b1a804
JamesLambert1986 <jameslambert1986@gmail.com>
2021-12-21 15:32:08 +0000

    Pull request

--- docs/views/elements/Container.vue
+++ docs/views/elements/Container.vue
@@ -1,23 +1,28 @@
 <template>
   <main>
-    <div class="container">
+    <div class="container pb-0">
       <ul class="breadcrumb mb-0 d-sm-none">
         <li><a href="/elements">Elements</a></li>
       </ul>
       <h1>Container</h1>
       <p>Containers are a fundamental building block that contain, pad, and align your content within a given device or viewport.</p>
-    </div>
-    <div class="container visualtest">
+
       <h2>Basic container</h2>
       <p>Our containers are simpler than the standard <a href="https://getbootstrap.com/docs/5.0/layout/containers/">bootstrap container</a> with less breakpoints. Padding is uniformed with a small bit of padding bottom added to help with the vertical rythm of the site. It's recommended to break up the content of the site into relevant containers and not to use a single page container.</p>
+    </div>
+    <div class="container visualtest">
       <span class="h5 mb-0 pb-0">Outer container</span>
       <div class="container-example mb-5">
         <div class="contents d-flex justify-content-center align-items-center"><span class="h5 mb-0 pb-0">Content</span></div>
-      </div>
+      </div>    
+    </div>
+    <div class="container">
       <h3>Container inside of a container</h3>
       <p>CSS rules have been created to prevent layouts to break when adding a container into another container; stopping too much padding being applied.</p>
       <h3>Backgrounds</h3>
       <p>Our theme colours can be applied the containers using a class looking like <strong>.bg-primary</strong>, making the container stand out more. Extra padding is then applied to container so the the content isn't squashed into the background. We also need to add some extra padding to the container that follows it so that there isn't any overlap and its clear that the next bit of content is seperate.</p>
+    </div>
+    <div class="container">
       <span class="h5 mb-0 pb-0">Outer container</span>
       <div class="container container-example bg-secondary">
         <div class="contents d-flex justify-content-center align-items-center"><span class="h5 mb-0 pb-0">Content</span></div>
@@ -44,7 +49,7 @@
       <span class="circle circle--cross text-white"></span>
     </div>
 
-    <div class="container visualtest">
+    <div class="container">
       <h2>Curved backgrounds</h2>
       <p>A curve can be added to the <strong>top</strong> of a container by adding a modfiying class like <strong>.container--curve</strong>. The container above will need to have enough padding added to the bottom to deal with the overlap, a <strong>.container--curve-below</strong> class can be used in this case.</p>
 
