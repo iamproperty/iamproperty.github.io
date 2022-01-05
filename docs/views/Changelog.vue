e5c381869499e42247fbcfc0f1e305b63e9387b0
JamesLambert1986 <jameslambert1986@gmail.com>
2022-01-05 20:41:28 +0000

    Changelog

--- docs/views/Changelog.vue
+++ docs/views/Changelog.vue
@@ -2,12 +2,12 @@
   <main>
     <div class="container">
       <h1>Changelog</h1>
-      <!--
+      
       <h2>V2.3.0 <span>(Future version)</span></h2>
       <ul>
-        <li></li>
+        <li>Inline YouTube video support</li>
       </ul>
-      -->
+
       <h2>V2.2.0 <span>(Current latest version)</span></h2>
       <ul>
         <li>Documentation update</li>
