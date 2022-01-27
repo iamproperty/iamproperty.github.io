3f9d9b4d2bce0c379979937ff3c9db3afd34a45d
James Fenwick <james.fenwick@iamproperty.com>
2022-01-27 17:08:39 +0000

    Add Composer package for Blade components
    DS-148

---
+++ blade/src/ServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace IAMProperty\iamkey;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    private const VIEW_PATH = __DIR__ . '/../resources/views';
+
+    public function boot(): void
+    {
+        $this->loadViewsFrom(self::VIEW_PATH, 'iamkey');
+
+        $this->publishes([
+            self::VIEW_PATH => resource_path('views/vendor/iamkey'),
+        ], 'views');
+    }
+}
