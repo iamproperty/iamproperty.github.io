feddc283758c46c31e64f8da52870139e731086b
JamesLambert1986 <jameslambert1986@gmail.com>
2022-09-26 10:12:42 +0100

    update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 // Modules
 import * as helpers from '../js/modules/helpers';
 import nav from '../js/modules/nav';
@@ -15,35 +16,35 @@ document.addEventListener("DOMContentLoaded", function () {
     helpers.checkElements(document.body);
     console.log('test.js');
     // ANav
-    Array.from(document.querySelectorAll('.nav')).forEach((arrayElement, index) => {
+    Array.from(document.querySelectorAll('.nav')).forEach((arrayElement) => {
         nav(arrayElement);
     });
     // Advanced tables
-    Array.from(document.querySelectorAll('.table__wrapper')).forEach((arrayElement, index) => {
+    Array.from(document.querySelectorAll('.table__wrapper')).forEach((arrayElement) => {
         table(arrayElement);
     });
     // Accordions
-    Array.from(document.querySelectorAll('.accordion')).forEach((arrayElement, index) => {
+    Array.from(document.querySelectorAll('.accordion')).forEach((arrayElement) => {
         accordion(arrayElement);
     });
     // Testimonial
-    Array.from(document.querySelectorAll('.testimonial')).forEach((arrayElement, index) => {
+    Array.from(document.querySelectorAll('.testimonial')).forEach((arrayElement) => {
         testimonial(arrayElement);
     });
     // Carousel
-    Array.from(document.querySelectorAll('.carousel')).forEach((arrayElement, index) => {
+    Array.from(document.querySelectorAll('.carousel')).forEach((arrayElement) => {
         carousel(arrayElement);
     });
     // Form
-    Array.from(document.querySelectorAll('form')).forEach((arrayElement, index) => {
+    Array.from(document.querySelectorAll('form')).forEach((arrayElement) => {
         form(arrayElement);
     });
     // Modal
-    Array.from(document.querySelectorAll('.modal')).forEach((arrayElement, index) => {
+    Array.from(document.querySelectorAll('.modal')).forEach((arrayElement) => {
         modal(arrayElement);
     });
     // YouTube videos
-    Array.from(document.querySelectorAll('.youtube-embed')).forEach((arrayElement, index) => {
+    Array.from(document.querySelectorAll('.youtube-embed')).forEach((arrayElement) => {
         new youtubeVideo(arrayElement);
     });
     window.addEventListener('hashchange', function () {
