90d4a0ab434b0c5f2722cef3a5ea0ac54d15ff8d
James Lambert <jameslambert1986@gmail.com>
2023-04-20 12:08:10 +0100

    fix build

--- docs/views/GetStarted.vue
+++ docs/views/GetStarted.vue
@@ -3,17 +3,23 @@
     <div class="container">
       <Readme></Readme>
       <h2>Workflow</h2>
-      <img src="development-workflow.png" alt-=""/>
+      <img :src="workflow" alt=""/>
     </div>
   </main>
 </template>
 
 <script>
 import Readme from '../../README.md'
+import workflow from '../img/development-workflow.png'
 
 export default {
   components: {
     Readme
+  },
+  data: function () {
+    return {
+      workflow: workflow
+    }
   }
 }
 </script>
--- docs/views/standalone/IamsoldAdmin.vue
+++ docs/views/standalone/IamsoldAdmin.vue
@@ -150,7 +150,6 @@ import Nav from '@/components/Nav/Nav.vue'
 import Input from '@/components/Input/Input.vue'
 import Carousel from '@/components/Carousel/Carousel.vue'
 import Table from '@/components/Table/Table.vue'
-import Drawer from '@/components/Drawer/Drawer.vue'
 
 export default {
   components: {
@@ -158,8 +157,7 @@ export default {
     Nav,
     Input,
     Carousel,
-    Table,
-    Drawer
+    Table
   },
   props: {
     searchTerm: {
