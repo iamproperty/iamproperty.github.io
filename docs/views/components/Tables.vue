a2b4199201180f8460c3fea3e226ac6575732286
James Lambert <jameslambert1986@gmail.com>
2023-05-25 10:34:05 +0100

    Refine the documentation

--- docs/views/components/Tables.vue
+++ docs/views/components/Tables.vue
@@ -161,109 +161,349 @@
     
     <div class="container">
       <h2>Max height table</h2>
-      <p>...</p>
+      <p>To control how much real estate the table takes up on a page we can set a max height from a pre-defined list. Using the class of <b>.table--mh-small</b> will set the max height to {{$shared.cssVars.table_mh_sm}} ({{$shared.cssVars.table_mh_sm_px}}px); <b>.table--mh-medium</b> will set it to {{$shared.cssVars.table_mh_md}} ({{$shared.cssVars.table_mh_md_px}}px) and <b>.table--mh-large</b> will set it to {{$shared.cssVars.table_mh_lg}} ({{$shared.cssVars.table_mh_lg_px}}px). Commonly when using one of these classes we don't want to convert the table into a stackable view on mobile, this is achievable by adding the **.table--fullwidth** class.</p>
     </div>
 
 
-    <div class="container visualtest">
-      
-      <Table class="table-mh-small">
-        <table>
-          <thead>
-            <tr>
-              <th>Client</th>
-              <th>Date added</th>
-              <th>Lorum ipsum</th>
-              <th>Lorum ipsum</th>
-              <th>Lorum ipsum</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Tom Smith</td>
-              <td>10.05.23</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td><a href="/">Manage client</a></td>
-            </tr>
-            <tr>
-              <td>Tom Smith</td>
-              <td>10.05.23</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td><a href="/">Manage client</a></td>
-            </tr>
-            <tr>
-              <td>Tom Smith</td>
-              <td>10.05.23</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td><a href="/">Manage client</a></td>
-            </tr>
-            <tr>
-              <td>Tom Smith</td>
-              <td>10.05.23</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td><a href="/">Manage client</a></td>
-            </tr>
-            <tr>
-              <td>Tom Smith</td>
-              <td>10.05.23</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td><a href="/">Manage client</a></td>
-            </tr>
-            <tr>
-              <td>Tom Smith</td>
-              <td>10.05.23</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td><a href="/">Manage client</a></td>
-            </tr>
-            <tr>
-              <td>Tom Smith</td>
-              <td>10.05.23</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td><a href="/">Manage client</a></td>
-            </tr>
-            <tr>
-              <td>Tom Smith</td>
-              <td>10.05.23</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td><a href="/">Manage client</a></td>
-            </tr>
-            <tr>
-              <td>Tom Smith</td>
-              <td>10.05.23</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td><a href="/">Manage client</a></td>
-            </tr>
-            <tr>
-              <td>Tom Smith</td>
-              <td>10.05.23</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td>Lorum ipsum</td>
-              <td><a href="/">Manage client</a></td>
-            </tr>
-          </tbody>
-        </table>
-      </Table>
-    </div>
+    <Tabs class="container">
+      <details>
+        <summary><h3>Max height small</h3></summary>
+        <Table class="table--mh-small table--fullwidth">
+          <table>
+            <thead>
+              <tr>
+                <th>Client</th>
+                <th>Date added</th>
+                <th>Lorum ipsum</th>
+                <th>Lorum ipsum</th>
+                <th>Lorum ipsum</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+            </tbody>
+          </table>
+        </Table>
+      </details>
+      <details>
+        <summary><h3>Max height medium</h3></summary>
+        <Table class="table--mh-medium table--fullwidth">
+          <table>
+            <thead>
+              <tr>
+                <th>Client</th>
+                <th>Date added</th>
+                <th>Lorum ipsum</th>
+                <th>Lorum ipsum</th>
+                <th>Lorum ipsum</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+            </tbody>
+          </table>
+        </Table>
+      </details>
+      <details>
+        <summary><h3>Max height large</h3></summary>
+        <Table class="table--mh-large table--fullwidth">
+          <table>
+            <thead>
+              <tr>
+                <th>Client</th>
+                <th>Date added</th>
+                <th>Lorum ipsum</th>
+                <th>Lorum ipsum</th>
+                <th>Lorum ipsum</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+              <tr>
+                <td>Tom Smith</td>
+                <td>10.05.23</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td>Lorum ipsum</td>
+                <td><a href="/">Manage client</a></td>
+              </tr>
+            </tbody>
+          </table>
+        </Table>
+      </details>
+    </Tabs>
 
     
     <div class="container">
@@ -332,21 +572,24 @@
       </div>
     </div>
 
-    <Tabs class="container">
-      <details>
-        <summary><h2>Web component</h2></summary>
-        <WebReadme></WebReadme>
-      </details>
-      <details>
-        <summary><h2>Vue component</h2></summary>
-        <Readme></Readme>
-      </details>
-      <details>
-        <summary><h2>HTML</h2></summary>
-        <pre><code class="javascript">{{htmlUsage}}</code></pre>
-      </details>
-    </Tabs>
-
+    <div class="container">
+      
+      <h2>Implementation</h2>
+      <Tabs >
+        <details>
+          <summary><h2>Web component</h2></summary>
+          <WebReadme></WebReadme>
+        </details>
+        <details>
+          <summary><h2>Vue component</h2></summary>
+          <Readme></Readme>
+        </details>
+        <details>
+          <summary><h2>HTML</h2></summary>
+          <pre><code class="javascript">{{htmlUsage}}</code></pre>
+        </details>
+      </Tabs>
+    </div>
     <div class="bg-admin version-control">
       <div class="container">
         <table>
@@ -358,6 +601,11 @@
             </tr>
           </thead>
           <tbody class="text-body">
+            <tr>
+              <td>V1.1</td>
+              <td>25.05.2023</td>
+              <td>Max heights have been enabled</td>
+            </tr>
             <tr>
               <td>V1 added</td>
               <td>15.05.2023</td>
