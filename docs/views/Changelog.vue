3455306a663448b64361c9f933d09f53974caff6
James Lambert <jameslambert1986@gmail.com>
2023-05-31 08:53:05 +0100

    Update Changelog.vue

--- docs/views/Changelog.vue
+++ docs/views/Changelog.vue
@@ -5,6 +5,7 @@
 
       <h2>V3.4.7--alpha</h2>
       <ul>
+        <li>Applied filters component</li>
         <li>Implement the datalayer</li>
         <li>New card custom element</li>
         <li>Removal of the card deck vue component</li>
