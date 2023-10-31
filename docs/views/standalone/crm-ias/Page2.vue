16d13fbd2c5f02bdeec31aa3c86c48d94f5a0c8d
JamesLambert1986 <jameslambert1986@gmail.com>
2023-10-31 11:38:24 +0000

    Update Page2.vue

--- docs/views/standalone/crm-ias/Page2.vue
+++ docs/views/standalone/crm-ias/Page2.vue
@@ -18,13 +18,13 @@
 
         <h3 class="h4">Payment routes</h3>
         <div class="row">
-          <div class="col-3"><img src="/img/estate-agents.png" alt="" class="mb-3"/></div>
+          <div class="col-3"><img src="/img/illustrations/estate-agents.png" alt="" class="mb-3"/></div>
           <div class="col">
             <p><strong>Buyer paid route</strong><br/> Upon successful sale, the buyer will be charged a fee of: 4.20% to a minimum of £6,000.00 (This fee covers the auction and estate agent contract.)</p>
           </div>
         </div>
         <div class="row">
-          <div class="col-3"><img src="/img/estate-agents.png" alt="" class="mb-3"/></div>
+          <div class="col-3"><img src="/img/illustrations/estate-agents.png" alt="" class="mb-3"/></div>
           <div class="col">
             <p><strong>Vendor paid route</strong><br/> Upon successful sale, the vendor will be charged a fee of: 4.20% to a minimum of £6,000.00.<br/><br/>The buyer is charged a deposit, which is taken from as part-payment of the final negotiated selling price and will be deducted from the balance of the final selling price upon completion of contracts.</p>
           </div>
@@ -33,13 +33,13 @@
         
         <h3 class="h4">Auction routes</h3>
         <div class="row">
-          <div class="col-3"><img :src="estateAgents" alt="" class="mb-3"/></div>
+          <div class="col-3"><img src="/img/illustrations/estate-agents.png" alt="" class="mb-3"/></div>
           <div class="col">
             <p><strong>Modern method</strong><br/> Buyer will pay the reservation fee or deposit on the property.<br/><br/> Agree to exchange contracts and complete within 56 days of receiving draft contracts.</p>
           </div>
         </div>
         <div class="row">
-          <div class="col-3"><img :src="estateAgents" alt="" class="mb-3"/></div>
+          <div class="col-3"><img src="/img/illustrations/estate-agents.png" alt="" class="mb-3"/></div>
           <div class="col">
             <p><strong>Traditional method</strong><br/> Buyer will pay a 10% deposit against the property.<br/><br/> Immediate exchange of contracts. Complete within 28 days from exchange.</p>
           </div>
