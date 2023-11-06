b5e0d171a13dc023b23ccdb5d70a041e9776b467
JamesLambert1986 <jameslambert1986@gmail.com>
2023-11-06 14:18:50 +0000

    Update slider.spec.ts

--- assets/ts/tests/slider.spec.ts
+++ assets/ts/tests/slider.spec.ts
@@ -7,27 +7,27 @@ import "expect-puppeteer";
 
 import iamSlider from "../components/slider/slider.component";
 
-if (!window.customElements.get(`iam-slider`))
-    window.customElements.define(`iam-slider`, iamSlider);
-
 describe('The slider component', () => {
 
-  const table = document.createElement('table');
-    table.innerHTML = basicTable;
-
-  tableModule.addDataAttributes(table);
+  if (!window.customElements.get(`iam-slider`))
+    window.customElements.define(`iam-slider`, iamSlider);
 
   test('should always show the minimum and maximum values that it can be set', () => {
 
     document.body.innerHTML = `<label>Input field label <iam-slider><input type="number" name="percent" min="0" max="100" value="15" step="1" /></iam-slider></label>`;
-
     let component = document.querySelector('iam-slider');
 
-console.log(component.shadowRoot);
-
-
-    expect(document.body.innerHTML).toContain('It works!')
-
+    expect(component.shadowRoot.innerHTML).toContain('<div class="col min pe-2">0</div>')
+    expect(component.shadowRoot.innerHTML).toContain('<div class="col max ps-2">100</div>')
   });
 
+  test('should always have a secondary way of inputing the value i.e. a number input field', () => {
+
+    document.body.innerHTML = `<label>Input field label <iam-slider><input type="number" name="percent" min="0" max="100" value="15" step="1" /></iam-slider></label>`;
+    let component = document.querySelector('iam-slider');
+
+    expect(component.querySelectorAll('input[type="number"]').length).toEqual(1)
+    expect(component.shadowRoot.querySelectorAll('input[type="range"]').length).toEqual(1)
+  });
+    
 });
