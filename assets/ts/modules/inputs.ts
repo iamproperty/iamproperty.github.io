90e82456c88396fad58dd625b2eac1e2ee559048
JamesLambert1986 <jameslambert1986@gmail.com>
2023-11-10 12:35:52 +0000

    Add date validation

--- assets/ts/modules/inputs.ts
+++ assets/ts/modules/inputs.ts
@@ -1,14 +1,71 @@
 // @ts-nocheck
 const extendInputs = (body) => {
 
-  document.addEventListener("load", function() {
-
+  function loadInput(){
     // maxlength counter init
-    Array.from(document.querySelectorAll('input')).forEach((input,index) => {
+    Array.from(document.querySelectorAll('input[maxlength]')).forEach((input,index) => {
       let wrapper = input.parentElement;
       setMaxlengthVars(input,wrapper);
     });
-  });
+
+    // Date restrictions 
+    if(document.querySelector('input[type="date"]')){
+
+      const today = new Date();
+      
+      function formatDate(date) {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+      }
+
+      Array.from(document.querySelectorAll('input[type="date"]')).forEach((input,index) => {
+        
+        let startDate = today;
+
+        if(input.hasAttribute('data-start')){
+
+          startDate.setDate(startDate.getDate() + parseInt(input.getAttribute('data-start')));
+          input.setAttribute('min', formatDate(startDate));
+        }
+
+        if(input.hasAttribute('data-period')){
+
+          let timePeriod = parseInt(input.getAttribute('data-period'));
+
+          let endDate = new Date();
+          endDate.setDate(startDate.getDate() + timePeriod);
+
+          input.setAttribute('max', formatDate(endDate));
+        }
+
+        if(input.hasAttribute('data-allowed-days')){
+
+          let allowedDays = JSON.parse(`[${input.getAttribute('data-allowed-days')}]`);
+          
+          input.addEventListener('input', function(e){
+            var day = new Date(this.value).getUTCDay();
+
+            if(allowedDays.includes(day))
+              input.setCustomValidity("");
+            else
+              input.setCustomValidity("That day of the week is not allowed");
+          });
+        }
+      });
+    }
+  }
+
+  if(document.readyState === 'complete') {
+    loadInput();
+  }
+
+  document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+      loadInput();
+    }
+  };
 
   body.addEventListener('input', (event) => {
     if (event && event.target instanceof HTMLElement && event.target.closest('input,textarea,select')){
