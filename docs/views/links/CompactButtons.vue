f42f11af949527409968fea054c190577abd0ceb
JamesLambert1986 <jameslambert1986@gmail.com>
2025-02-28 13:39:17 +0000

    Update CompactButtons.vue

--- docs/views/links/CompactButtons.vue
+++ docs/views/links/CompactButtons.vue
@@ -81,8 +81,8 @@
       </div>
     </div>
 
-    <h2>Small</h2>
-
+    <h3>Small</h3>
+    <p>Use a small primary compact button when there isn’t enough space for a default compact primary compact button.</p>
     <div class="container visualtest pb-5">
       <div class="row">
         <div class="col-5 col-sm-3 pb-3 pe-none">
