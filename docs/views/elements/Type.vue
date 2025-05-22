78061310f3e1960155c44e63f60eeaadd4eb9008
JamesLambert1986 <jameslambert1986@gmail.com>
2025-05-22 11:19:09 +0100

    Update heading sizes

--- docs/views/elements/Type.vue
+++ docs/views/elements/Type.vue
@@ -14,167 +14,70 @@
         We use Qanelas font for all in-product experiences. This ensures that the UI is optimised to be highly legible,
         performs well, and is frictionless as you move between iamproperty products and the rest of the system.
       </p>
+      <p>We use a type scale ratio of 1.2, minor third scale, to ensure consistency through type sizing.</p>
     </div>
     <div class="container visualtest">
-      <h3>Headings - Mobile</h3>
 
       <div class="row">
         <div class="d-none d-md-block col-md-3">
+          <img :src="tabletDesktopImg" alt="Rendered image of a tablet and a desktop PC" loading="lazy" class="mb-5" />
           <img :src="mobileImg" alt="Rendered image of a mobile phone" loading="lazy" />
         </div>
         <div class="col">
           <div class="row">
             <div class="col-12 col-sm-7 mb-2">
-              <span
-                class="h1"
-                :style="`font-size:${$shared.cssVars.h1_fs_rem}!important;line-height:${$shared.cssVars.h1_lh_rem}!important;padding-bottom:${$shared.cssVars.h1_pb_rem}!important;`"
-                >This is heading 1<span
+              <span class="h1">This is heading 1<span
                   class="lineheight-demo"
-                  :style="`height:${$shared.cssVars.h1_pb_rem}!important`"
-                ></span
-              ></span>
-            </div>
-            <div class="col-12 col-sm-4 mb-5 ms-auto">
-              <span class="small extra-small"
-                >H1<br />Font-family: Qanelas<br />Font-weight: Bold<br />Font-size: {{ $shared.cssVars.h1_fs_rem }} ({{
-                  $shared.cssVars.h1_fs
-                }}px)<br />Letter-spacing: 0px<br />Line-height: {{ $shared.cssVars.h1_lh_rem }} ({{
-                  $shared.cssVars.h1_lh
-                }}px)<br />Padding-bottom: {{ $shared.cssVars.h1_pb_rem }} ({{ $shared.cssVars.h1_pb }}px)</span
-              >
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-12 col-sm-7 mb-2">
-              <span
-                class="h2"
-                :style="`font-size:${$shared.cssVars.h2_fs_rem}!important;line-height:${$shared.cssVars.h2_lh_rem}!important;padding-bottom:${$shared.cssVars.h2_pb_rem}!important;`"
-                >This is heading 2<span
-                  class="lineheight-demo"
-                  :style="`height:${$shared.cssVars.h2_pb_rem}!important`"
-                ></span
-              ></span>
-            </div>
-            <div class="col-12 col-sm-4 mb-5 ms-auto">
-              <span class="small extra-small"
-                >H2<br />Font-family: Qanelas<br />Font-weight: Bold<br />Font-size: {{ $shared.cssVars.h2_fs_rem }} ({{
-                  $shared.cssVars.h2_fs
-                }}px)<br />Letter-spacing: 0px<br />Line-height: {{ $shared.cssVars.h2_lh_rem }} ({{
-                  $shared.cssVars.h2_lh
-                }}px)<br />Padding-bottom: {{ $shared.cssVars.h2_pb_rem }} ({{ $shared.cssVars.h2_pb }}px)</span
-              >
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-12 col-sm-7 mb-2">
-              <span
-                class="h3"
-                :style="`font-size:${$shared.cssVars.h3_fs_rem}!important;line-height:${$shared.cssVars.h3_lh_rem}!important;padding-bottom:${$shared.cssVars.h3_pb_rem}!important;`"
-                >This is heading 3<span
-                  class="lineheight-demo"
-                  :style="`height:${$shared.cssVars.h3_pb_rem}!important`"
-                ></span
-              ></span>
-            </div>
-            <div class="col-12 col-sm-4 mb-5 ms-auto">
-              <span class="small extra-small"
-                >H3<br />Font-family: Qanelas<br />Font-weight: Bold<br />Font-size: {{ $shared.cssVars.h3_fs_rem }} ({{
-                  $shared.cssVars.h3_fs
-                }}px)<br />Letter-spacing: 0px<br />Line-height: {{ $shared.cssVars.h3_lh_rem }} ({{
-                  $shared.cssVars.h3_lh
-                }}px)<br />Padding-bottom: {{ $shared.cssVars.h3_pb_rem }} ({{ $shared.cssVars.h3_pb }}px)</span
-              >
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-12 col-sm-7 mb-2">
-              <span
-                class="h4"
-                :style="`font-size:${$shared.cssVars.h4_fs_rem}!important;line-height:${$shared.cssVars.h3_lh_rem}!important;padding-bottom:${$shared.cssVars.h4_pb_rem}!important;`"
-                >This is heading 4<span
-                  class="lineheight-demo"
-                  :style="`height:${$shared.cssVars.h4_pb_rem}!important`"
-                ></span
-              ></span>
-            </div>
-            <div class="col-12 col-sm-4 mb-5 ms-auto">
-              <span class="small extra-small"
-                >H4<br />Font-family: Qanelas<br />Font-weight: Semi-Bold<br />Font-size:
-                {{ $shared.cssVars.h4_fs_rem }} ({{ $shared.cssVars.h4_fs }}px)<br />Letter-spacing: 0px<br />Line-height:
-                {{ $shared.cssVars.h4_lh_rem }} ({{ $shared.cssVars.h4_lh }}px)<br />Padding-bottom:
-                {{ $shared.cssVars.h4_pb_rem }} ({{ $shared.cssVars.h4_pb }}px)</span
-              >
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="container visualtest">
-      <h3>Headings - Tablet and Desktop</h3>
-
-      <div class="row">
-        <div class="d-none d-md-block col-md-3">
-          <img :src="tabletDesktopImg" alt="Rendered image of a tablet and a desktop PC" loading="lazy" />
-        </div>
-        <div class="col">
-          <div class="row">
-            <div class="col-12 col-sm-7 mb-2">
-              <span
-                class="h1"
-                :style="`font-size:${$shared.cssVars.h1_fs_sm_rem}!important;line-height:${$shared.cssVars.h1_lh_sm_rem}!important;padding-bottom:${$shared.cssVars.h1_pb_sm_rem}!important;`"
-                >This is heading 1<span
-                  class="lineheight-demo"
-                  :style="`height:${$shared.cssVars.h1_pb_sm_rem}!important`"
+                  :style="`height:${$shared.cssVars.h1_pb}!important`"
                 ></span
               ></span>
             </div>
             <div class="col-12 col-sm-4 mb-5 ms-auto">
               <span class="small extra-small"
                 >H1<br />Font-family: Qanelas<br />Font-weight: Bold<br />Font-size:
-                {{ $shared.cssVars.h1_fs_sm_rem }} ({{ $shared.cssVars.h1_fs_sm }}px)<br />Letter-spacing: 0px<br />Line-height:
-                {{ $shared.cssVars.h1_lh_sm_rem }} ({{ $shared.cssVars.h1_lh_sm }}px)<br />Padding-bottom:
-                {{ $shared.cssVars.h1_pb_sm_rem }} ({{ $shared.cssVars.h1_pb_sm }}px)</span
+                3rem (48px)<br />Letter-spacing: 0px<br />Line-height:
+                3.5rem (56px)<br />Padding-bottom:
+                3.5rem (56px)</span
               >
             </div>
+            
           </div>
           <div class="row">
             <div class="col-12 col-sm-7 mb-2">
               <span
-                class="h2"
-                :style="`font-size:${$shared.cssVars.h2_fs_sm_rem}!important;line-height:${$shared.cssVars.h2_lh_sm_rem}!important;padding-bottom:${$shared.cssVars.h2_pb_sm_rem}!important;`"
-                >This is heading 2<span
+                class="h2">This is heading 2<span
                   class="lineheight-demo"
-                  :style="`height:${$shared.cssVars.h2_pb_sm_rem}!important`"
+                  :style="`height:${$shared.cssVars.h2_pb}!important`"
                 ></span
               ></span>
             </div>
             <div class="col-12 col-sm-4 mb-5 ms-auto">
               <span class="small extra-small"
                 >H2<br />Font-family: Qanelas<br />Font-weight: Bold<br />Font-size:
-                {{ $shared.cssVars.h2_fs_sm_rem }} ({{ $shared.cssVars.h2_fs_sm }}px)<br />Letter-spacing: 0px<br />Line-height:
-                {{ $shared.cssVars.h2_lh_sm_rem }} ({{ $shared.cssVars.h2_lh_sm }}px)<br />Padding-bottom:
-                {{ $shared.cssVars.h2_pb_sm_rem }} ({{ $shared.cssVars.h2_pb_sm }}px)</span
+                2.5rem (40px)<br />Letter-spacing: 0px<br />Line-height:
+                3rem (48px)<br />Padding-bottom:
+                3rem (48px)</span
               >
             </div>
+            
           </div>
           <div class="row">
             <div class="col-12 col-sm-7 mb-2">
               <span
                 class="h3"
-                :style="`font-size:${$shared.cssVars.h3_fs_sm_rem}!important;line-height:${$shared.cssVars.h3_lh_sm_rem}!important;padding-bottom:${$shared.cssVars.h3_pb_sm_rem}!important;`"
+                :style="`font-size:${$shared.cssVars.h3_fs}!important;line-height:${$shared.cssVars.h3_lh}!important;padding-bottom:${$shared.cssVars.h3_pb}!important;`"
                 >This is heading 3<span
                   class="lineheight-demo"
-                  :style="`height:${$shared.cssVars.h3_pb_sm_rem}!important`"
+                  :style="`height:${$shared.cssVars.h3_pb}!important`"
                 ></span
               ></span>
             </div>
             <div class="col-12 col-sm-4 mb-5 ms-auto">
               <span class="small extra-small"
-                >H3<br />Font-family: Qanelas<br />Font-weight: Bold<br />Font-size:
-                {{ $shared.cssVars.h3_fs_sm_rem }} ({{ $shared.cssVars.h3_fs_sm }}px)<br />Letter-spacing: 0px<br />Line-height:
-                {{ $shared.cssVars.h3_lh_sm_rem }} ({{ $shared.cssVars.h3_lh_sm }}px)<br />Padding-bottom:
-                {{ $shared.cssVars.h3_pb_sm_rem }} ({{ $shared.cssVars.h3_pb_sm }}px)</span
+                >H3<br />Font-family: Qanelas<br />Font-weight: Semi-Bold <br />Font-size:
+                1.75rem (28px)<br />Letter-spacing: 0px<br />Line-height:
+                2rem (36px)<br />Padding-bottom:
+                2rem (36px)</span
               >
             </div>
           </div>
@@ -182,19 +85,19 @@
             <div class="col-12 col-sm-7 mb-2">
               <span
                 class="h4"
-                :style="`font-size:${$shared.cssVars.h4_fs_sm_rem}!important;line-height:${$shared.cssVars.h3_lh_sm_rem}!important;padding-bottom:${$shared.cssVars.h4_pb_sm_rem}!important;`"
+                :style="`font-size:${$shared.cssVars.h4_fs}!important;line-height:${$shared.cssVars.h3_lh}!important;padding-bottom:${$shared.cssVars.h4_pb}!important;`"
                 >This is heading 4<span
                   class="lineheight-demo"
-                  :style="`height:${$shared.cssVars.h4_pb_sm_rem}!important`"
+                  :style="`height:${$shared.cssVars.h4_pb}!important`"
                 ></span
               ></span>
             </div>
             <div class="col-12 col-sm-4 mb-5 ms-auto">
               <span class="small extra-small"
                 >H4<br />Font-family: Qanelas<br />Font-weight: Semi-Bold<br />Font-size:
-                {{ $shared.cssVars.h4_fs_sm_rem }} ({{ $shared.cssVars.h4_fs_sm }}px)<br />Letter-spacing: 0px<br />Line-height:
-                {{ $shared.cssVars.h4_lh_sm_rem }} ({{ $shared.cssVars.h4_lh_sm }}px)<br />Padding-bottom:
-                {{ $shared.cssVars.h4_pb_sm_rem }} ({{ $shared.cssVars.h4_pb_sm }}px)</span
+                1.5rem (24px)<br />Letter-spacing: 0px<br />Line-height:
+                1.75rem (32px)<br />Padding-bottom:
+                1.75rem (32px)</span
               >
             </div>
           </div>
@@ -308,7 +211,7 @@
     <div class="container visualtest">
       <h2>Typography - colour</h2>
       <div class="colour-sample">
-        <div class="colour" :style="`background:${$shared.cssVars.themeColour_primary}!important`"></div>
+        <div class="colour bg-primary"></div>
         <span class="lead pb-2">Primary text colour</span><br />
         <span>CSS Variable: --colour-primary</span><br />
         <p>HEX code: {{ $shared.cssVars.themeColour_primary.toUpperCase() }}</p>
