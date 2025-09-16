4c0446a393a99cbc60559703595ee9c8fa0caf91
Jonathan O'Riordan <jonathan.o'riordan@iamproperty.com>
2025-09-16 10:15:17 +0100

    Principles page updated in line with design

--- docs/views/Principles.vue
+++ docs/views/Principles.vue
@@ -1,8 +1,73 @@
+<script setup>
+  import Card from '@/components/Card/Card.vue';
+  import cardImg from '../img/cardImg.png';
+
+</script>
 <template>
   <main>
-    <h1>Principles</h1>
-    <p class="strapline">Basic concepts and rules to help guide us to making the correct choices.</p>
-    <ul>
+    <h1 class="text-center">Design for our Customers</h1>
+    <strong class="text-center text-primary pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna alLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna al.</strong>
+
+    <div class="container visualtest colour-block bg-info gradient-primary">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mb-4 pe-none g-10">
+        <div class="col bg-white p-4 rounded shadow ">
+          <span class="pb-3 d-block text-uppercase">Intuitive</span>
+          <a href="/client-onbaording">
+            <Card>Make it intuitive</Card>
+          </a>
+        </div>
+
+        <div class="col bg-white p-4 rounded shadow">
+          <span class="pb-3 d-block text-uppercase">Empowering</span>
+          <a href="/client-onbaording">
+            <Card class="hover">Make it empowering</Card>
+          </a>
+        </div>
+
+        <div class="col bg-white p-4 rounded shadow">
+          <span class="pb-3 d-block text-uppercase">Delightful</span>
+          <a href="/client-onbaording">
+            <Card class="active">Make it Delightful</Card>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="container visualtest">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mb-4 pe-none">
+        <div class="col">
+          <span class="pb-3 d-block text-uppercase">Default State</span>
+          <a href="/client-onbaording">
+            <Card :data-image="cardImg"
+              >Financial preparation <span>Here would be the top level description of the task</span></Card
+            >
+          </a>
+        </div>
+
+        <div class="col">
+          <span class="pb-3 d-block text-uppercase">Hover/focus State</span>
+          <a href="/client-onbaording">
+            <Card :data-image="cardImg" class="hover"
+              >Financial preparation <span>Here would be the top level description of the task</span></Card
+            >
+          </a>
+        </div>
+
+        <div class="col">
+          <span class="pb-3 d-block text-uppercase">Active State</span>
+          <a href="/client-onbaording">
+            <Card :data-image="cardImg" class="active"
+              >Financial preparation <span>Here would be the top level description of the task</span></Card
+            >
+          </a>
+        </div>
+      </div>
+    </div>
+
+    
+
+    
+    <!-- <ul>
       <li class="pb-3">
         <strong class="text-primary d-block">Relative units:</strong> Keeping units relative means they can react and
         respond to their environment making them more flexibile and durable.
@@ -30,6 +95,6 @@
         application. Concatenate files where appropriate, only make ajax calls that would produce new valid information
         and lazy load assets.
       </li>
-    </ul>
+    </ul> -->
   </main>
 </template>
