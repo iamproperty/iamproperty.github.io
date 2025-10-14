f7c018d7ddc7dc22e3d4a02f3b0db9ab62c2b133
JamesLambert1986 <jameslambert1986@gmail.com>
2025-10-14 13:18:37 +0100

    Update inputs.ts

--- assets/ts/modules/inputs.ts
+++ assets/ts/modules/inputs.ts
@@ -2,13 +2,7 @@ import hibpCheck from '../vendor/hibp.js';
 
 const extendInputs = (body): void => {
   function loadInput(): void {
-    Array.from(document.querySelectorAll('label input')).forEach((input) => {
-      if (!input.closest('label').querySelector('.optional-text') && !input.hasAttribute('required')) {
-        if (input.parentNode.tagName.toLowerCase() == 'span')
-          input.parentElement.insertAdjacentHTML('beforebegin', `<span class="optional-text"></span>`);
-        else input.insertAdjacentHTML('beforebegin', `<span class="optional-text"></span>`);
-      }
-    });
+
 
     // Date restrictions
     if (document.querySelector('input[type="date"]')) {
