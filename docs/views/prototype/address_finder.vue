29eabe1053e8bba09f43f1a6aa31e52ad72e632b
JamesLambert1986 <jameslambert1986@gmail.com>
2025-12-02 16:46:40 +0000

    Update address_finder.vue

--- docs/views/prototype/address_finder.vue
+++ docs/views/prototype/address_finder.vue
@@ -5,11 +5,8 @@
 </script>
 
 <template>
-  <div class="was-validated">
-    <STDAddressLookup data-postcode-value="NE3 3HD" 
-                      data-county-required="" 
-                      data-use="{&quot;paf_id&quot;:&quot;1&quot;,&quot;paf_entry_type&quot;:&quot;Standard&quot;,&quot;date_added&quot;:&quot;2023-11-07T10:30:00&quot;,&quot;date_updated&quot;:&quot;&quot;,&quot;date_removed&quot;:&quot;&quot;,&quot;building_number&quot;:&quot;1&quot;,&quot;sub_building_name&quot;:&quot;&quot;,&quot;building_name&quot;:&quot;&quot;,&quot;organisation_name&quot;:&quot;&quot;,&quot;department_name&quot;:&quot;&quot;,&quot;po_box_number&quot;:&quot;&quot;,&quot;dependent_thoroughfare&quot;:&quot;&quot;,&quot;thoroughfare&quot;:&quot;Acacia Avenue&quot;,&quot;double_dependent_locality&quot;:&quot;&quot;,&quot;dependent_locality&quot;:&quot;Gosforth&quot;,&quot;post_town&quot;:&quot;NEWCASTLE-UPON-TYNE&quot;,&quot;postcode&quot;:&quot;NE3 3LU&quot;,&quot;country&quot;:{&quot;id&quot;:&quot;urn:als:country:a72ndjIq&quot;,&quot;name&quot;:&quot;United Kingdom of Great Britain and Northern Ireland&quot;,&quot;alpha-2&quot;:&quot;GB&quot;,&quot;alpha-3&quot;:&quot;GBR&quot;,&quot;is_fatf&quot;:false,&quot;short_name&quot;:&quot;United Kingdom&quot;},&quot;postcode_outward_code&quot;:&quot;NE3&quot;,&quot;postcode_inward_code&quot;:&quot;3LU&quot;,&quot;postcode_type&quot;:&quot;S&quot;,&quot;small_user_organisation&quot;:&quot;&quot;,&quot;delivery_point_suffix&quot;:&quot;&quot;,&quot;district&quot;:&quot;&quot;,&quot;ward&quot;:&quot;&quot;,&quot;uprn&quot;:&quot;123456789012&quot;,&quot;udprn&quot;:&quot;&quot;,&quot;umrrn&quot;:&quot;&quot;,&quot;usrn&quot;:&quot;&quot;,&quot;longitude&quot;:&quot;123&quot;,&quot;latitude&quot;:&quot;&quot;,&quot;label&quot;:&quot;1 Acacia Avenue, Gosforth, NEWCASTLE-UPON-TYNE, NE3 3LU, UNITED KINGDOM&quot;}"
-                      data-use-default=""
-    ></STDAddressLookup>
-  </div>
+  <form class="d-block pt-5">
+    <STDAddressLookup data-county-required="" data-country-required=""></STDAddressLookup>
+    <button class="btn btn-primary">Submit form</button>
+  </form>
 </template>
