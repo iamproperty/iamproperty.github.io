57893051c1597d5ac92c01525019c6bf3aacdc2e
JamesLambert1986 <jameslambert1986@gmail.com>
2026-06-17 09:39:33 +0100

    move the slot strategy for the nav

--- docs/views/standalone/eco/product.vue
+++ docs/views/standalone/eco/product.vue
@@ -2,11 +2,12 @@
   import Nav from '@/components/Nav/Nav.vue';
   import STDNav from '@/components/STDNav/STDNav.vue';
   import Card from '@/components/Card/Card.vue';
-  
+
 </script>
 <template>
-  <nav class="nav--sticky">
-    <Nav>
+  <nav>
+    <Nav data-account-btn-title="My account" data-account-btn-class="btn-compact" class="nav--sticky">
+
       <a href="/" class="brand brand--property" slot="logo">
         <svg>
           <title>iam key</title>
@@ -14,26 +15,53 @@
         </svg>
       </a>
 
-      
       <router-link to="/standalone/premium-conveyancing">Dashboard</router-link>
       <router-link to="/standalone/premium-conveyancing/cases">Cases</router-link>
       <router-link to="/standalone/premium-conveyancing/settings">Settings</router-link>
-      
 
-      <button class="btn btn-primary" data-modal="modal-transactional" slot="actions">My account</button>
 
+
+      <!--
       <STDNav slot="secondary">
-        
+
         <router-link to="/standalone/premium-conveyancing">Eco Hub</router-link>
-        
+
 
       </STDNav>
+      -->
+
+
+        <span class="branch me-auto" slot="account">Gosforth Agent & Co</span>
+        <BranchSelector slot="account">
+
+          <label><input type="checkbox" name="branch" value="123" />Gosforth Agent & co</label>
+          <label><input type="checkbox" name="branch" value="122" />Heaton Agent & co</label>
+          <label><input type="checkbox" name="branch" value="124" />Newcastle Agent & co</label>
+          <label><input type="checkbox" name="branch" value="125" />Jesmond Agent & co</label>
+
+        </BranchSelector>
+
+        <span class="title" slot="account">John Jones</span>
+        <a href="/" slot="account">Agency settings</a>
+        <a href="/" slot="account">Control panel</a>
+        <a href="/" slot="account">Contact us</a>
+
+      <router-link to="/standalone/premium-conveyancing" slot="secondary">Eco Hub</router-link>
     </Nav>
   </nav>
-  
+
   <main class="">
 
 
     <h1>product content</h1>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software including versions of Lorem Ipsum.</p>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software including versions of Lorem Ipsum.</p>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software including versions of Lorem Ipsum.</p>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software including versions of Lorem Ipsum.</p>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software including versions of Lorem Ipsum.</p>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software including versions of Lorem Ipsum.</p>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software including versions of Lorem Ipsum.</p>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software including versions of Lorem Ipsum.</p>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software including versions of Lorem Ipsum.</p>
   </main>
 </template>
